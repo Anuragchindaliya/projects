bdde2e15049eca3189fb4cba9aa99eaf2439f0f8
Anuragchindaliya <ak1148383@gmail.com>
2024-03-21 21:49:34 +0530

    fix: anchor tage issue

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -72,6 +72,7 @@ export const PinPerspective = ({
             href={href}
             target={"_blank"}
             className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
+            rel="noreferrer"
           >
             <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
               {title}
