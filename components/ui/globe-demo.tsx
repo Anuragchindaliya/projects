87d399f5af3ee00670bf74a11d221d5845f982f6
Anuragchindaliya <ak1148383@gmail.com>
2024-06-01 22:52:35 +0530

    fix:globe-demo type

--- components/ui/globe-demo.tsx
+++ components/ui/globe-demo.tsx
@@ -2,7 +2,7 @@
 import { motion } from "framer-motion";
 import dynamic from "next/dynamic";
 
-const World = dynamic(() => import("../ui/globe").then((m) => m.World), {
+const World = dynamic<any>(() => import("../ui/globe").then((m) => m.World), {
   ssr: false,
 });
 
